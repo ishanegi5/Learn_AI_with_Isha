a33e76f1c6606c404d13c0bccef677a000466a8e
Isha Negi <officialisha095@gmail.com>
2026-06-14 12:00:23 +0530

    day 5 code

---
+++ day2.py
@@ -0,0 +1,9 @@
+# print("Hello world")
+# name=input("Enter your name: ")
+# print("My name is: ",name)
+# print(f"My name is : {name}")
+
+age=int(input("enter age: ")) #5.0
+# print(type(age))
+# print(age+5) #5.0+5=10.0
+print(type(age))
