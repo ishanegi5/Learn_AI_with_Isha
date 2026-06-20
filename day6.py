53800f09ef326ccb340c2c36daa2d1ad82efca07
Isha Negi <officialisha095@gmail.com>
2026-06-20 16:27:35 +0530

    indexing and slicing

---
+++ day6.py
@@ -0,0 +1,46 @@
+#indexing
+#list
+"""l=[2,4,5,6,8,3,5]
+print(l[0])
+print(l[-1])
+print(l[2])
+#tuple
+t=(14,6,8)
+print(t[-1])
+
+#string
+s="isha"
+print(s[1])
+"""
+#slicing
+#string
+s="isha negi"
+print(s[3:]) #a negi
+print(s[:4]) #isha
+
+#tuple
+t=(67,45,34,787,44,22)
+print(t[:3])
+print(t[2::2])
+
+#list,string,tuple
+l=[20,24,45,65,68,63,75]
+
+#start,stop ->default
+print(l[1:4])
+
+#start,stop,step
+print(l[1:4:2])
+
+#reverse
+print(l[::-1])
+print(t[::-1])
+print(s[::-1])
+
+print(s[9::-2]) 
+"""
+NOTE:
+    #start->inclusive
+    #stop->exclusive
+
+"""
