b8189342a5567c0acacff1b10b58c137a3037e9e
Isha Negi <officialisha095@gmail.com>
2026-06-26 07:36:27 +0530

    conditional statements

---
+++ day7.py
@@ -0,0 +1,20 @@
+"""
+#even-odd
+num=int(input("Enter num: "))
+if(num%2==0):
+    print("even")
+else:
+    print("odd")
+
+
+"""
+#marks-grades
+marks=int(input("Enter marks: "))
+if(marks>=90):
+    print("A")
+elif(marks>=60):
+    print("B")
+elif(marks>=40):
+    print("C")
+else:
+    print("D")
