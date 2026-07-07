7469af7dd63fa6ed6a65e758c6a7e841d4091117
Isha Negi <officialisha095@gmail.com>
2026-07-07 18:31:16 +0530

    modules

---
+++ custom_m.py
@@ -0,0 +1,7 @@
+radius=2.5 #variable
+
+def circle_circum(radius):  #function
+    return 2*3.14*radius
+
+class Shape: #class
+    pass
---
+++ day10.py
@@ -0,0 +1,29 @@
+#modules
+"""1)built-in -> eg: random,math,os
+   2)custom module ->eg: .py file   
+
+
+
+#built-n
+#1 math
+import math
+print(math.sqrt(4))
+
+#2 random
+import random
+print(random.randint(1,10))
+
+#3 os
+import os
+print(os.getcwd()) #get current working directory
+
+
+#custom module
+import custom_m as cm
+print(cm.radius) #varible
+print(cm.circle_circum(2)) #function
+obj=cm.Shape() #class instance
+
+"""
+from math import sqrt
+print(sqrt(5))
