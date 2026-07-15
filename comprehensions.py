91ec2e415cbff52b50d1951b1f0e9deb4f3b8121
Isha Negi <officialisha095@gmail.com>
2026-07-15 19:29:14 +0530

    this is comprehension

---
+++ comprehensions.py
@@ -0,0 +1,15 @@
+"""
+#list 
+square=[x**2 for x in range(5)]
+print(square)
+
+
+#dictionary
+diction={x: x**2 for x in range(5)}
+print(diction)
+
+"""
+
+#filtering
+fil=[x for x in range(10) if x%2!=0]
+print(fil)
